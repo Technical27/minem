fa006aede5a932a59738ca6506270d0c2043571f
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-12-25 20:36:06 -0500

    added global config file

--- minem.js
+++ minem.js
@@ -6,6 +6,7 @@ const chalk = require('chalk');
 const cli = require('commander');
 const Promise = require('bluebird');
 
+const os = require('os');
 const path = require('path');
 const fs = Promise.promisifyAll(require('fs'));
 
@@ -22,6 +23,7 @@ const logger = winston.createLogger({
     }
   })
 });
+
 const defaultConfig = `{
   "serverFile": "server.jar",
   "serverDir": "server",
@@ -30,6 +32,12 @@ const defaultConfig = `{
     "max": "2G"
   }
 }`;
+
+const globalConfigPath = path.join(os.homedir(), '.minem.json');
+
+if (!fs.existsSync(globalConfigPath)) {
+  fs.writeFileSync(globalConfigPath, '{}');
+}
 
 cli.version('1.1.6', '-v, --version');
 
@@ -41,11 +49,25 @@ cli
     fs.writeFileAsync('minem.json', defaultConfig)
       .then(() => {
         logger.log('info', 'creating server directory');
-        return fs.mkdirAsync('server');
+        if (!fs.existsSync('server')) return fs.mkdirAsync('server');
       })
       .then(() => {
         logger.log('info', 'creating eula.txt');
+        logger.log('info', '(you agree to this): https://account.mojang.com/documents/minecraft_eula');
         return fs.writeFileAsync(path.join('server', 'eula.txt'), 'eula=true');
+      })
+      .then(() => {
+        return fs.readFileAsync(globalConfigPath);
+      })
+      .then(file => {
+        const globalConfig = JSON.parse(file);
+        if (!globalConfig.servers) globalConfig.servers = [];
+
+        const name = path.basename(process.cwd()); 
+        if (!globalConfig.servers.some(s => s.name === name)) {
+          globalConfig.servers.push({name, path: process.cwd(), status: 'offline'});
+          return fs.writeFileAsync(globalConfigPath, JSON.stringify(globalConfig));
+        }
       })
       .catch(e => logger.log('error', e));
   });
